5d938e3408a75608c8ad843c0940fd7b1f74ab49
Shogo Mark <trigmark87@gmail.com>
2026-09-19 17:51:31 +0100

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-/// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'node',
-    globals: true,
-    setupFiles: './src/test/setup.ts',
-  },
 })
